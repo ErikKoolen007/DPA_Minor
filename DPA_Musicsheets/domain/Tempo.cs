ae02acfe87ac2d993aa36707358a124493bd1695
Lennard Slabbekoorn <lennard12@hotmail.nl>
2019-01-11 21:48:37 +0100

    attempt to implement tempo

---
+++ DPA_Musicsheets/domain/Tempo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.domain
+{
+    class Tempo : MusicPart
+    {
+        private int bpm { get; set; }
+        private int number { get; set; }
+        private int tempo { get; set; }
+
+        public Tempo(int bpm, int number)
+        {
+            this.bpm = bpm;
+            this.number = number;
+        }
+
+        public override string ToString()
+        {
+            return "\\tempo 4 = " + bpm;
+        }
+
+        private void calculateTempo()
+        {
+            //not implemented yet
+        }
+    }
+}
