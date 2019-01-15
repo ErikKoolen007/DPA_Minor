a318049bb707d1de7595b83601ab28066346151a
Lennard Slabbekoorn <lennard12@hotmail.nl>
2019-01-15 21:53:14 +0100

    Nearly finishd convert to midi

--- DPA_Musicsheets/adapters/EndTrackAdaptee.cs
+++ DPA_Musicsheets/adapters/EndTrackAdaptee.cs
@@ -11,26 +11,17 @@ namespace DPA_Musicsheets.adapters
 {
     class EndTrackAdaptee : MetaTypeAdapter
     {
-        private int _beatNote;
-        private int _beatsPerBar;
-        private int _division;
-        private int _prevNoteAbsoluteTicks;
-        private double _prctBarBarReached;
+        private int _percentageOfBar;
         private MidiEvent _ev;
 
-        public EndTrackAdaptee(MetaMessage msg, int beatNote, int beatsPerBar, int division, int prevNoteAbsoluteTicks, double prctBarReached, MidiEvent ev)
+        public EndTrackAdaptee(MetaMessage msg, MidiEvent ev, int percentageOfBar)
         {
             if (msg.MetaType != MetaType.EndOfTrack)
                 throw new TypeAccessException();
 
             this.msg = msg;
-            _beatNote = beatNote;
-            _beatsPerBar = beatsPerBar;
-            _division = division;
-            _prevNoteAbsoluteTicks = prevNoteAbsoluteTicks;
-            _prctBarBarReached = prctBarReached;
             _ev = ev;
-
+            _percentageOfBar = percentageOfBar;
             decodeMsg();
         }
         public override MusicPart LoadIntoDomain()
@@ -40,23 +31,23 @@ namespace DPA_Musicsheets.adapters
 
         public sealed override void decodeMsg()
         {
-            if (_prevNoteAbsoluteTicks > 0)
-            {
-                // Finish the last notelength.
-                double percentageOfBar;
-                MidiToLilyHelper.GetLilypondNoteLength(_prevNoteAbsoluteTicks, _ev.AbsoluteTicks, _division,
-                    _beatNote, _beatsPerBar, out percentageOfBar);
-                //lilypondContent.Append(MidiToLilyHelper.GetLilypondNoteLength(previousNoteAbsoluteTicks, midiEvent.AbsoluteTicks, _division, _beatNote, _beatsPerBar, out percentageOfBar));
-                //lilypondContent.Append(" ");
-
-                _prctBarBarReached += percentageOfBar;
-                if (_prctBarBarReached >= 1)
-                {
-
-                    //lilypondContent.AppendLine("|");
-                    percentageOfBar = percentageOfBar - 1;
-                }
-            }
+//            if (_prevNoteAbsoluteTicks > 0)
+//            {
+//                // Finish the last notelength.
+//                double percentageOfBar;
+//                MidiToLilyHelper.GetLilypondNoteLength(_prevNoteAbsoluteTicks, _ev.AbsoluteTicks, _division,
+//                    _beatNote, _beatsPerBar, out percentageOfBar);
+//                //lilypondContent.Append(MidiToLilyHelper.GetLilypondNoteLength(previousNoteAbsoluteTicks, midiEvent.AbsoluteTicks, _division, _beatNote, _beatsPerBar, out percentageOfBar));
+//                //lilypondContent.Append(" ");
+//
+//                _prctBarBarReached += percentageOfBar;
+//                if (_prctBarBarReached >= 1)
+//                {
+//
+//                    //lilypondContent.AppendLine("|");
+//                    percentageOfBar = percentageOfBar - 1;
+//                }
+//            }
         }
     }
 }
--- DPA_Musicsheets/factories/MidiFactory.cs
+++ DPA_Musicsheets/factories/MidiFactory.cs
@@ -15,10 +15,11 @@ namespace DPA_Musicsheets.factories
         private string file_name;
         private Sequence seq;
 
-        private bool startedNoteIsClosed = true;
-        private int previousMidiKey = 60; // Central C;
-        private int previousNoteAbsoluteTicks = 0;
-        private double percentageOfBarReached = 0;
+        double _percentageOfBar;
+        private bool _startedNoteIsClosed = true;
+        private int _previousMidiKey = 60; // Central C;
+        private int _previousNoteAbsoluteTicks = 0;
+        private double _percentageOfBarReached = 0;
         public int _beatNote { get; set; }
         public int _beatsPerBar { get; set; }
         public MidiFactory(string file_name)
@@ -37,7 +38,7 @@ namespace DPA_Musicsheets.factories
             seq.Load(file_name);
         }
 
-        private MusicPart loadChannelMsg(ChannelMessage msg, MidiEvent e)
+        private MusicPart LoadChannelMsg(ChannelMessage msg, MidiEvent e)
         {
             BaseNote note = new Note("", "");
             int division = seq.Division;
@@ -47,41 +48,45 @@ namespace DPA_Musicsheets.factories
                 if (msg.Data2 > 0) // Data2 = loudness
                 {
                     // Append the new baseNote.
-                    note.letter = MidiToLilyHelper.GetLilyNoteName(previousMidiKey, msg.Data1);
-                    previousMidiKey = msg.Data1;
-                    startedNoteIsClosed = false;
+                    //note.letter = MidiToLilyHelper.GetLilyNoteName(_previousMidiKey, msg.Data1);
+                    note = TranslateMidiName(msg.Data1 % 12, note);
+
+                    _previousMidiKey = msg.Data1;
+                    _startedNoteIsClosed = false;
                 }
-                else if (!startedNoteIsClosed)
+                else if (!_startedNoteIsClosed)
                 {
                     // Finish the previous baseNote with the length.
-                    double percentageOfBar;
-                    note.duration = MidiToLilyHelper.GetLilypondNoteLength(previousNoteAbsoluteTicks, e.AbsoluteTicks, division, _beatNote, _beatsPerBar, out percentageOfBar);
-                    previousNoteAbsoluteTicks = e.AbsoluteTicks;
+                    
+                    //note.duration = MidiToLilyHelper.GetLilypondNoteLength(_previousNoteAbsoluteTicks, e.AbsoluteTicks, division, _beatNote, _beatsPerBar, file_namepercentageOfBar);
+                    note.duration = "" + TranslateMidiDuration(e.AbsoluteTicks, division, note);
+                    _previousNoteAbsoluteTicks = e.AbsoluteTicks;
                     
                     BaseNoteSpace noteSpace = new BaseNoteSpace(note);
                     note = noteSpace;
                    // lilypondContent.Append(" ");
 
-                    percentageOfBarReached += percentageOfBar;
-                    if (percentageOfBarReached >= 1)
+                    _percentageOfBarReached += _percentageOfBar;
+                    if (_percentageOfBarReached >= 1)
                     {
                         BaseNoteMark noteMark = new BaseNoteMark(note);
                         note = noteMark;
                         //lilypondContent.AppendLine("|");
-                        percentageOfBarReached -= 1;
+                        _percentageOfBarReached -= 1;
                     }
-                    startedNoteIsClosed = true;
+                    _startedNoteIsClosed = true;
                 }
                 else
                 {
                     //lilypondContent.Append("r");
+                    Rest r = new Rest();
                 }
             }
 
             return null;
         }
 
-        private MusicPart loadMetaMsg(MetaMessage msg)
+        private MusicPart LoadMetaMsg(MetaMessage msg)
         {
             MetaTypeAdapter adapter;
             switch (msg.MetaType)
@@ -96,6 +101,147 @@ namespace DPA_Musicsheets.factories
             }
 
             return null;
+        }
+
+        private BaseNote TranslateMidiName(int midiKey, BaseNote note)
+        {
+            BaseNoteCross crossNote = null;
+
+            switch (midiKey % 12)
+            {
+                case 0:
+                    note.letter = "c";
+                    break;
+                case 1:
+                    note.letter = "c";
+                    crossNote = new BaseNoteCross(note);
+                    note = crossNote;
+                    break;
+                case 2:
+                    note.letter = "d";
+                    break;
+                case 3:
+                    note.letter = "d";
+                    crossNote = new BaseNoteCross(note);
+                    note = crossNote;
+                    break;
+                case 4:
+                    note.letter = "e";
+                    break;
+                case 5:
+                    note.letter = "f";
+                    break;
+                case 6:
+                    note.letter = "f";
+                    crossNote = new BaseNoteCross(note);
+                    note = crossNote;
+                    break;
+                case 7:
+                    note.letter = "g";
+                    break;
+                case 8:
+                    note.letter = "g";
+                    crossNote = new BaseNoteCross(note);
+                    note = crossNote;
+                    break;
+                case 9:
+                    note.letter = "a";
+                    break;
+                case 10:
+                    note.letter = "a";
+                    crossNote = new BaseNoteCross(note);
+                    note = crossNote;
+                    break;
+                case 11:
+                    note.letter = "b";
+                    break;
+            }
+
+            return note;
+        }
+
+        private int TranslateMidiDuration(int absoluteTicks, int division, BaseNote note)
+        {
+            int duration = 0;
+            int dots = 0;
+
+            //is this correct?
+            double deltaTicks = _previousNoteAbsoluteTicks - absoluteTicks;
+
+            if (deltaTicks <= 0)
+            {
+                _percentageOfBar = 0;
+                return 0;
+            }
+
+            double percentageOfBeatNote = deltaTicks / division;
+            _percentageOfBar = (1.0 / _beatsPerBar) * percentageOfBeatNote;
+
+            for (int noteLength = 32; noteLength >= 1; noteLength -= 1)
+            {
+                double absoluteNoteLength = (1.0 / noteLength);
+
+                if (_percentageOfBar <= absoluteNoteLength)
+                {
+                    if (noteLength < 2)
+                        noteLength = 2;
+
+                    int subtractDuration;
+
+                    if (noteLength == 32)
+                        subtractDuration = 32;
+                    else if (noteLength >= 16)
+                        subtractDuration = 16;
+                    else if (noteLength >= 8)
+                        subtractDuration = 8;
+                    else if (noteLength >= 4)
+                        subtractDuration = 4;
+                    else
+                        subtractDuration = 2;
+
+                    if (noteLength >= 17)
+                        duration = 32;
+                    else if (noteLength >= 9)
+                        duration = 16;
+                    else if (noteLength >= 5)
+                        duration = 8;
+                    else if (noteLength >= 3)
+                        duration = 4;
+                    else
+                        duration = 2;
+
+                    double currentTime = 0;
+
+                    while (currentTime < (noteLength - subtractDuration))
+                    {
+                        var addtime = 1 / ((subtractDuration / _beatNote) * Math.Pow(2, dots));
+                        if (addtime <= 0) break;
+                        currentTime += addtime;
+                        if (currentTime <= (noteLength - subtractDuration))
+                        {
+                            dots++;
+                        }
+                        if (dots >= 4) break;
+                        note = WrapDots(dots, note);
+                    }
+                    
+                    break;
+                }
+            }
+
+            return duration;
+        }
+
+        private BaseNote WrapDots(int numberDots, BaseNote note)
+        {
+            BaseNoteDot dotNote = null;
+            for (int i = 0; i < numberDots; i++)
+            {
+                dotNote = new BaseNoteDot(note);
+                note = dotNote;
+            }
+
+            return note;
         }
     }
 }
